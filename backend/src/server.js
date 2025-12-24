1648e65a4dea31ad2f5459fef65e3628e70df51a
Arseyillion <echukwuemeka017@gmail.com>
2025-12-24 13:11:05 +0100

    correcting path resolution and environment set up errors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,13 @@
 import express from "express"
 import path from "path"
 import { ENV } from "./config/env.js"
+import { fileURLToPath } from "url"
 
 const app = express()
 
-const __dirname = path.resolve()
+// Get the full path of the current file 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.resolve(__filename)
 
 app.get("/api/health",(req, res)=>{
     res.status(200).json({message:"success"})
@@ -17,8 +20,8 @@ if(ENV.NODE_ENV === "production"){
 
     // if we visit any routes other than our api routes we would like to see our application
     app.get("/{*any}",(req, res)=>{
-        res.sendFile(path.join(__dirname, "../admin", "index.html"))
+        res.sendFile(path.join(__dirname, "../admin/dist", "index.html"))
     })
 }
 
-app.listen(5000,()=> console.log("server is up and running... "))
+app.listen(ENV.PORT,()=> console.log("server is up and running... "))
