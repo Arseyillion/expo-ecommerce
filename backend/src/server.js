1ebccbdcc3ca18630541fa507abab21ac12c089f
Arseyillion <echukwuemeka017@gmail.com>
2025-12-30 00:48:14 +0100

    order routes and controllers added

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,8 +7,13 @@ import { serve } from "inngest/express";
 import { functions, inngest } from "./config/inngest.js";
 import { syncUser, deleteUserFromDB } from "./config/inngest.js";
 
-import adminRoutes from "./routes/admin.routes.js";
-import userRoutes from "./routes/user.routes.js";
+import adminRoutes from "./routes/admin.route.js";
+import userRoutes from "./routes/user.route.js";
+import orderRoutes from "./routes/order.route.js";
+import reviewRoutes from "./routes/review.route.js";
+import productRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
+import paymentRoutes from "./routes/payment.route.js";
 
 const app = express();
 
@@ -28,6 +33,11 @@ app.use(
 
 app.use("/api/admin",adminRoutes)
 app.use("/api/users",userRoutes)
+app.use("/api/orders", orderRoutes);
+app.use("/api/reviews", reviewRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+
 
 
 app.get("/api/health", (req, res) => {
