92b16b49026e32bdc0232e924eba9d0237e8abd2
Arseyillion <echukwuemeka017@gmail.com>
2026-01-02 23:33:05 +0100

    feat: integrate Sentry for error tracking and created CustomersPage with improved customer display

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -9,6 +9,22 @@ import { Toaster } from 'sonner'
 // added after tanstack installaton
 import { QueryClient, QueryClientProvider} from '@tanstack/react-query'
 
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+  enableLogs: true,
+  integrations: [new Sentry.replayIntegration()],
+  // session replay
+  replaysSessionSampleRate: 1.0,
+  // This sets the sample rate to 10% for sessions that have errors. You may want to set it to 100% while in development and sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0,
+});
+
+
 // import your publishable key 
 const PUBLISHABLE_KEY= import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
